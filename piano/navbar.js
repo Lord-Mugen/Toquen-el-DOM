070e8104695fa3c725beee2e6fb6461ef1cdd907
ElCabal <cabalholguinlufe@gmail.com>
2022-01-23 22:43:34 -0500

    página piano y navbar menú hamburguesa

---
+++ piano/navbar.js
@@ -0,0 +1,12 @@
+/* document.querySelector seleciona todo tipo de elementos dentro del documento de html */
+
+let navbar = document.querySelector(".navbar");
+let navbarList = document.querySelector(".navbar__list");
+let menuButton = document.getElementById("menu");
+
+menuButton.addEventListener("click", openMenu);
+
+function openMenu() {
+  navbar.classList.toggle("active");
+  navbarList.classList.toggle("active");
+}
